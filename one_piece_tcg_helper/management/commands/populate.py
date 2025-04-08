43b2f883ab064bfafcfe205aa312bff2cd088ef9
Derrick <dsoriano109@gmail.com>
2025-04-08 00:08:16 -0700

    create command directory to set up admin commands references #1

---
+++ one_piece_tcg_helper/management/commands/populate.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand, CommandError
+
+class Command(BaseCommand):
+    help = "populates card info from tcgcsv"
+
+    def handle(self):
+        pass
+
+
+
+
