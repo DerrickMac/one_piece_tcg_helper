83d5cc2e585b092b23d0a4a5997c4060fe5c7276
Derrick <dsoriano109@gmail.com>
2025-04-07 21:55:42 -0700

    Register PriceHistory in admin References #2

--- database/admin.py
+++ database/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Leader, Character, Stage, Event
+from .models import Leader, Character, Stage, Event, PriceHistory
 
 admin.site.register(Leader)
 admin.site.register(Character)
 admin.site.register(Stage)
 admin.site.register(Event)
+admin.site.register(PriceHistory)
